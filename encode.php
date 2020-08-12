41a9f5072c8f5bca3ba3ac1447afd6082d061019
Max Base <MaxBaseCode@Gmail.Com>
2020-08-13 00:45:15 +0430

    Create encode.php

---
+++ encode.php
@@ -0,0 +1,24 @@
+<?php
+// https://github.com/BaseMax/php-jwt-example
+require 'vendor/autoload.php';
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\SignatureInvalidException;
+$key = '83aeee518617ad6f3393bf0685e37d3e';
+
+// JSON
+$data=[
+  "status"=>"success",
+  "data1"=>"test1",
+  "data2"=>"test2",
+  "data3"=>"test3",
+  "data4"=>"test4",
+  "data5"=>"test5",
+];
+$data = json_encode($data);
+$jwt = JWT::encode($data, $key, 'HS256');
+print $jwt."\n";
+
+// plain text
+$data = "Hello, World!";
+$jwt = JWT::encode($data, $key, 'HS256');
+print $jwt."\n";
