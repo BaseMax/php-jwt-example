168958ce40f35b16073ea157ab9a2eee6e0b7229
Max Base <MaxBaseCode@Gmail.Com>
2020-08-13 15:51:22 +0430

    Change HS256 to HS512, and remove json example

--- encode.php
+++ encode.php
@@ -7,18 +7,5 @@ $key = '83aeee518617ad6f3393bf0685e37d3e';
 
 // plain text
 $data = "Hello, World!";
-$jwt = JWT::encode($data, $key, 'HS256');
-print $jwt."\n";
-
-// JSON
-$data=[
-  "status"=>"success",
-  "data1"=>"test1",
-  "data2"=>"test2",
-  "data3"=>"test3",
-  "data4"=>"test4",
-  "data5"=>"test5",
-];
-$data = json_encode($data);
-$jwt = JWT::encode($data, $key, 'HS256');
+$jwt = JWT::encode($data, $key, 'HS512');
 print $jwt."\n";
